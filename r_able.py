541fb8a018692489326d0252f616146aa9097d1d
Przemek2829 <przemek2829@wp.pl>
2023-11-22 20:48:00 +0100

    Version 1.0.0

--- r_able.py
+++ r_able.py
@@ -1,3 +1,20 @@
+"""
+| ----------------------------------------------------------------------------------------------------------------------
+| Date                : November 2023
+| Copyright           : © 2023 by The Wroclaw Institute of Spatial Information and Artificial Intelligence
+| Email               : biuro@wizipisi.ai
+|
+| This file is part of the R-ABLE plugin and r_able python package.
+|
+| This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+| License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+|
+| This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+| warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+|
+| You should have received a copy of the GNU General Public License (COPYING.txt). If not see www.gnu.org/licenses.
+| ----------------------------------------------------------------------------------------------------------------------
+"""
 import os.path
 
 from qgis.PyQt.QtCore import Qt, QTimer, QSettings, QTranslator, QCoreApplication
@@ -21,8 +38,6 @@ class RAble:
             locale = QSettings().value('locale/userLocale')[0:2]
         except:
             locale = 'en'
-        print(locale)
-        # locale = 'en'
         locale_path = os.path.join(
             self.plugin_dir,
             'i18n',
